3160e7b2687b9195bac6f0b8724e0610f5df43aa
Amirul Zin <amirul.mdzin@gmail.com>
2015-10-23 03:00:07 +0800

    Source import for library module

---
+++ pillar/src/main/java/com/baseconfig/pillar/drawertype/base/BaseDrawerActivity.java
@@ -0,0 +1,121 @@
+package com.baseconfig.pillar.drawertype.base;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.FrameLayout;
+
+import com.baseconfig.pillar.R;
+
+public abstract class BaseDrawerActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private FrameLayout mFrameLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private NavigationView mNavigationView;
+    private NavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener;
+
+    public NavigationView getNavigationView() {
+        return mNavigationView;
+    }
+
+    public ActionBarDrawerToggle getDrawerToggle() {
+        return mDrawerToggle;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS | Window.FEATURE_ACTIVITY_TRANSITIONS);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pillar_layout_base_drawer);
+        setupViews(savedInstanceState);
+    }
+
+    public NavigationView.OnNavigationItemSelectedListener getOnNavigationItemSelectedListener() {
+        return mOnNavigationItemSelectedListener;
+    }
+
+    private void setupViews(final Bundle savedInstanceState) {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.pillar_lbd_drawerlayout);
+        mFrameLayout = (FrameLayout) findViewById(R.id.pillar_lbd_contentcontainer);
+        mNavigationView = (NavigationView) findViewById(R.id.pillar_lbd_navigationview);
+        mOnNavigationItemSelectedListener = new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                if (itemId != getDrawerItemId()) {
+                    getActivityFactory().changeActivityFromDrawer(BaseDrawerActivity.this, itemId);
+                }
+                mDrawerLayout.closeDrawers();
+                return false;
+            }
+        };
+        mNavigationView.setNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    /**
+     * For concrete implementation return an ID here that corresponds with the navigation item menu id that was set in the NavigationView menu resource.
+     */
+    public abstract int getDrawerItemId();
+
+    public abstract ActivityFactory getActivityFactory();
+
+    public View inflateContent(@LayoutRes int layoutResourceId) {
+        View inflatedView = LayoutInflater.from(BaseDrawerActivity.this).inflate(layoutResourceId, getContentContainer(), false);
+        getContentContainer().addView(inflatedView);
+        return inflatedView;
+    }
+
+    public FrameLayout getContentContainer() {
+        return mFrameLayout;
+    }
+
+    public DrawerLayout getDrawerLayout() {
+        return mDrawerLayout;
+    }
+
+    public void setDrawerNavigationButton(@NonNull Toolbar toolbar) {
+        if (mDrawerLayout != null) {
+            mDrawerToggle = new ActionBarDrawerToggle(BaseDrawerActivity.this, mDrawerLayout, toolbar, R.string.pillar_acc_drawer_open, R.string.pillar_acc_drawer_close);
+            mDrawerLayout.setDrawerListener(mDrawerToggle);
+            mDrawerToggle.syncState();
+        }
+    }
+
+    public interface ActivityFactory {
+        void changeActivityFromDrawer(Context context, @IdRes int menuItemId);
+    }
+
+    /**
+     * A Fragment who optionally depends on {@link BaseDrawerActivity} for communicating calls.
+     */
+    public abstract static class DrawerFragment extends Fragment {
+
+        public void setDrawerNavigationButton(@NonNull Toolbar toolbar) {
+            final FragmentActivity activity = getActivity();
+            if (activity != null && activity instanceof BaseDrawerActivity) {
+                ((BaseDrawerActivity) activity).setDrawerNavigationButton(toolbar);
+
+            }
+        }
+
+        public String getFragmentTag() {
+            return getClass().getSimpleName();
+        }
+    }
+}
---
+++ pillar/src/main/java/com/baseconfig/pillar/drawertype/base/RecyclerHomeBaseActivity.java
@@ -0,0 +1,67 @@
+package com.baseconfig.pillar.drawertype.base;
+
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.baseconfig.pillar.R;
+
+
+public abstract class RecyclerHomeBaseActivity extends BaseDrawerActivity {
+    private RecyclerView mRecyclerView;
+    private CollapsingToolbarLayout mCollapsingToolbarLayout;
+    private ImageView mHeaderImageView;
+    private Toolbar mTitleToolbar;
+
+    public Toolbar getTitleToolbar() {
+        return mTitleToolbar;
+    }
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final View view = inflateContent(R.layout.pillar_layout_base_recycler_collapsible);
+        innerSetupViews(view);
+        setupView(savedInstanceState, view);
+    }
+
+    public abstract void setupView(Bundle savedInstanceState, View baseLayout);
+
+    public RecyclerView getRecyclerView() {
+        return mRecyclerView;
+    }
+
+    public CollapsingToolbarLayout getCollapsingToolbarLayout() {
+        return mCollapsingToolbarLayout;
+    }
+
+    public ImageView getHeaderImageView() {
+        return mHeaderImageView;
+    }
+
+    private void innerSetupViews(View baseLayout) {
+        mTitleToolbar = (Toolbar) baseLayout.findViewById(R.id.lbnc_toolbar);
+        mHeaderImageView = (ImageView) baseLayout.findViewById(R.id.lbnc_headerimage);
+
+        mCollapsingToolbarLayout = (CollapsingToolbarLayout) baseLayout.findViewById(R.id.lbnc_collapsingtoolbar);
+        mRecyclerView = (RecyclerView) baseLayout.findViewById(R.id.lbnc_recyclerview);
+
+    }
+
+    public void applySpecialOverlay() {
+        final Drawable drawable;
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP_MR1) {
+            drawable = getResources().getDrawable(R.drawable.pillar_gradient_shadow_top);
+        } else
+            drawable = getResources().getDrawable(R.drawable.pillar_gradient_shadow_top, getTheme());
+
+        getCollapsingToolbarLayout().setForeground(drawable);
+
+    }
+
+}
