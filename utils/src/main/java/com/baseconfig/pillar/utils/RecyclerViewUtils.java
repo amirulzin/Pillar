13b6ba5b6c63af17c695374bb2115d5bf7f46a3d
Amirul Zin <amirul.mdzin@gmail.com>
2017-08-14 18:37:15 +0800

    Add helper DP constructor to VerticalItemDecoration class

--- utils/src/main/java/com/baseconfig/pillar/utils/RecyclerViewUtils.java
+++ utils/src/main/java/com/baseconfig/pillar/utils/RecyclerViewUtils.java
@@ -1,26 +1,34 @@
 package com.baseconfig.pillar.utils;
 
+import android.content.Context;
 import android.graphics.Rect;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 public class RecyclerViewUtils {
 
+    /**
+     * This class will add basic (and the correct) pixel margin to each list item in a vertical RecyclerView.
+     */
     public static class VerticalItemDecoration extends RecyclerView.ItemDecoration {
-        private int space;
+        private int pixelMargin;
 
-        public VerticalItemDecoration(int space) {
-            this.space = space;
+        public VerticalItemDecoration(int pixelMargin) {
+            this.pixelMargin = pixelMargin;
+        }
+
+        public VerticalItemDecoration(Context context, int dpMargin) {
+            this.pixelMargin = ContextUtils.getPixels(context, dpMargin);
         }
 
         @Override
         public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
 
-            if (parent.getChildAdapterPosition(view) == 0) outRect.top = space;
+            if (parent.getChildAdapterPosition(view) == 0) outRect.top = pixelMargin;
 
-            outRect.bottom = space;
-            outRect.left = space;
-            outRect.right = space;
+            outRect.bottom = pixelMargin;
+            outRect.left = pixelMargin;
+            outRect.right = pixelMargin;
 
         }
     }
